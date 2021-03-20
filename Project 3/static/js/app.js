84b22b1290671f99cb5a1a912000743d571af05e
pnisenson <pnisenson@gmail.com>
2021-03-20 13:53:08 -0400

    map working and attached, updated bar chart with title logos

--- Project 3/static/js/app.js
+++ Project 3/static/js/app.js
@@ -2,8 +2,6 @@
 function init() {
     var selector = d3.select("#selDataset");
     d3.json("data/fastfood.json").then((data) => {
-        console.log(data)
-
         var foodChain = data.features;
         const FoodChain = [];
         foodChain.forEach((item)=> {
@@ -15,7 +13,6 @@ function init() {
         //sort the array before rendering
         sortedArr=FoodChain.sort();
         sortedArr.forEach((item)=> {
-            console.log(item)
             selector
                 .append("option")
                 .text(item)
@@ -26,24 +23,15 @@ function init() {
     // var sortArr = FoodChain.sort();
     // console.log(sortArr);
     createChart(sortedArr[0]); 
-    createScatter(sortedArr[0])
-    
-    
+    createScatter(sortedArr[0]);
+    createMap();    
 });
 
 }
 
-d3.csv("data/Census_data.csv").then((census)=>{
-    console.log(census)
-    console.log("snails")
-})
-
 census = d3.csv("data/Census_data.csv")
-console.log(census)
 
 d3.json("data/third.json").then((third)=>{
-    console.log(third)
-    console.log("snails")
       var mcdonalds = [];
         foodChain.forEach((item)=> {
             i=item.properties;
@@ -52,31 +40,11 @@ d3.json("data/third.json").then((third)=>{
                 }
         })
 })
-// // function createDemographics(id){
-// //     d3.json("data/samples.json").then((data) => {
-// //         var metadata= data.metadata;
-// //         var filteredId = metadata.filter(d => d.id.toString() === id);
-// //         showDemographics(filteredId);
-    
-// //     });
-// // }  
-// // function showDemographics(metadata){
-// //     demographics = d3.select("#sample-metadata")
-// //     //clear the panel 
-// //     demographics.html('');
-// //     //get the key/value pair
-// //     Object.entries(metadata[0]).forEach(([key, value]) => {
-// //         console.log(key);
-// //         demographics.append("h5").text(`${key} : ${value}`)
-// //     })
-// // }
 
 function createChart(id){
-    console.log("Big5", id)
     d3.json("data/fastfood.json").then((data) => {
         var foodChain = data.features;
         var filtered = foodChain.filter(d => d.properties.Big4.toString() === id);
-        console.log(filtered);
         var big5=[], province=[], big4=[], population=[];
         filtered.forEach((item)=> {
             province.push(item.properties.province); 
@@ -97,40 +65,45 @@ function createChart(id){
         });
 
         //Top ten locations 
-        console.log(states);
         sorted_scores=scores.sort(function(a,b){return b-a;});
         var toptenProvince = states
                             .slice(0, 10)
                             .reverse()
                             .map(st => st);
-        console.log(toptenProvince);
         var toptenScores= scores
                            .slice(0, 10)
                            .reverse();
                         //    .sort(function(a,b){return b-a;});
-        console.log(toptenScores);
         // //Hover text
          var labels = "locations";
         // Build Bar charts 
         switch(id){
-                case "Burger King":
-                  color = "rgb(24, 84, 148)";
-                  break;
-                case "Taco Bell":
-                  color= "rgb(104, 42, 141)";
-                  break;
-                case "McDonalds":
-                  color= "rgb(255 199 44)";
-                  break;
-                case "Subway":
-                  color= "rgb(0, 140, 21)";
-                  break;
-                case "Wendys":
-                  color= "rgb(221, 20, 56)";
-                  break;
-                default:
-                  color: 'rgb(106, 83, 184)'
-              }
+          case "Burger King":
+            color = "rgb(24, 84, 148)";
+            title = "Burger King";
+            image = "static/images/chart/bk.png"
+            break;
+          case "Taco Bell":
+            color= "rgb(104, 42, 141)";
+            title = "Taco Bell";
+            image = "static/images/chart/tb.png"
+            break;
+          case "McDonalds":
+            color= "rgb(255 199 44)";
+            title = "McDonald's";
+            image = "static/images/chart/Mcdonalds.png"
+            break;
+          case "Subway":
+            color= "rgb(0, 140, 21)";
+            title = "Subway";
+            image = "static/images/chart/subway.png"
+            break;
+          case "Wendy's":
+            color= "rgb(221, 20, 56)";
+            title = "Wendys";
+            image = "static/images/chart/Wendys.png"
+            break;
+          }
         var trace = {
             x: toptenScores,
             y: toptenProvince,
@@ -140,22 +113,35 @@ function createChart(id){
             type:"bar",
             orientation: "h"
             };
-         var layout = {
-             title: "<b>Fast food chains by State</b>",
-             height: 500,
-		  	 width: 600,
+        var layout = {
+            title: "<b>"+"Locations By State"+"</b>",
+            height: 500,
+            width: 600,
              "titlefont": {
                 "size": 20
               },
-             yaxis: {
+            yaxis: {
                  tickmode: "linear",
              },
-             margin: {
+            margin: {
                 l: 100,
                 r: 100,
                 t: 50,
                 b: 20
-            }
+            },
+            images: [
+              {
+                x: 0.1,
+                y: 1.02,
+                sizex: 0.1,
+                sizey: 0.1,
+                source: image,
+                xanchor: "right",
+                xref: "paper",
+                yanchor: "bottom",
+                yref: "paper"
+              }
+            ],
          };
         Plotly.newPlot("bar", [trace], layout)
     })
@@ -163,28 +149,7 @@ function createChart(id){
 
 function createScatter(id){
     d3.json("data/third.json").then((data) => {
-        console.log(data.data.BK)
         var foodChain = data.data;
-        console.log(foodChain.Population)
-        // var counts = []
-        //       switch(id){
-        //         case "Burger King":
-        //           counts.push(foodChain.BK);
-        //           break;
-        //         case "Taco Bell":
-        //           counts.push(foodChain.TB);
-        //           break;
-        //         case "McDonalds":
-        //           counts.push(foodChain.McDonalds);
-        //           break;
-        //         case "Subway":
-        //           counts.push(foodChain.Subway)
-        //           break;
-        //         case "Wendys":
-        //           counts.push(foodChain.Wendys)
-        //           break;
-        //       }
-        //console.log(counts);
         var province=[], pairs = [], one = [], two = [];
         foodChain.forEach((item)=> {
             province.push(item.Name); 
@@ -213,10 +178,6 @@ function createScatter(id){
             pairs.push([ratio, poverty])
 
         })
-        console.log(one)
-        console.log(two)
-        console.log(province)
-        console.log(pairs)
         var options={
         series: [{
           name: "SAMPLE A",
@@ -246,74 +207,9 @@ function createScatter(id){
         var chart = new ApexCharts(document.querySelector("#splatter"), options);
         chart.render();
 
-    //     var trace1 = {
-    //       x: two,
-    //       y: one,
-    //       mode: 'markers',
-    //       type: 'scatter'
-    //     };
-    //     var data = [trace1]
-    //     Plotly.newPlot("splatter", data)
-    // })
+
 })
 }
-//********************************************************************** */   
-//         //Build Bubble charts
-//         var trace1 = {
-//            x: filtered.otu_ids,
-//            y: filtered.sample_values,
-//            text: filtered.otu_labels,
-//            mode: "markers",
-//            marker: {
-//             size: filtered.sample_values,
-//             color:filtered.otu_ids,
-//             colorscale: "Earth"
-//             },
-           
-//         };
-//         var bubble_layout = {
-//             title: "<br><b>Bubble Chart OTU id vs Sample values</b>",
-//             xaxis: {title: "OTU IDs"},
-//             height : 600,
-//             width: 1000
-//         }
-//         Plotly.newPlot(bubble, [trace1], bubble_layout)
-//         // Build guage chart
-//         var metadata = data.metadata.filter(d => d.id.toString() === id)[0];
-//         var wfreq = metadata.wfreq;
-//         if (wfreq == null) {
-//             wfreq = 0;
-//         }
-//         var data = [
-//             {
-//                 domain: { x: [0, 1], y: [0, 1]  },            
-//                 value: parseFloat(wfreq),
-//                 title: '<b>Belly Button Washing Frequency</b><br>Scrubs per Week<br><br>',
-// 		  	    height: 600,
-// 		  	    width: 600,
-//                 type: "indicator",
-//                 mode: "gauge+number",
-//                 gauge: { axis: { range: [0, 9] },
-//                 bar: { color: 'rgba(8,29,88,0)' },
-//                    steps: [
-//                     { range: [0, 1], color: 'rgb(255,255,217)' },
-//                     { range: [1, 2], color: 'rgb(237,248,217)' },
-//                     { range: [2, 3], color: 'rgb(199,233,180)' },
-//                     { range: [3, 4], color: 'rgb(127,205,187)' },
-//                     { range: [4, 5], color: 'rgb(65,182,196)' },
-//                     { range: [5, 6], color: 'rgb(29,145,192)' },
-//                     { range: [6, 7], color: 'rgb(34,94,168)' },
-//                     { range: [7, 8], color: 'rgb(37,52,148)' },
-//                     { range: [8, 9], color: 'rgb(8,29,88)' }
-//                   ]}
-//             }
-//         ];
-        
-//         var layout = { width: 600, height: 500, margin: { t: 10, b: 0 },
-//                         };
-//         Plotly.newPlot('gauge', data, layout);
-// });
-// }
 
 function optionChanged(id) {
     console.log("In OptionChanged "+ id);
@@ -349,105 +245,109 @@ function arrCount(states) {
 
 // Heatmap code
 // // create map object
-// var myMap = L.map("map", {
-//   center: [37.0902, -95.7129],
-//   zoom: 7
-// });
-
-// // Add a tile layer
-// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//   tileSize: 512,
-//   maxZoom: 18,
-//   zoomOffset: -1,
-//   id: "mapbox/light-v9",
-//   accessToken: API_KEY
-// }).addTo(myMap);
-
+function createMap(){
+console.log("try again")
+var myMap = L.map("map", {
+  center: [37.0902, -95.7129],
+  zoom: 7
+});
+// Add a tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/light-v9",
+  accessToken: API_KEY
+}).addTo(myMap);
+console.log("try 2")
 // Heatmap layer
 // Assign the Data json to a variable
-// var geodata = "static/data/cities.json";
+var geodata = "static/data/cities.json";
+// Read the data using D3
 
-// // Read the data using D3
-// d3.json(geodata, function(response) {
+d3.json(geodata).then((response) =>{
+  console.log(response)
 
-//   console.log(response.features.length);
+  console.log(response.features.length);
 
-//   var heatArray = [];
+  var heatArray = [];
 
-//   for (var i = 0; i < response.features.length; i++) {
-//     var location = response.features[i].geometry.coordinates;
-//     var dense = response.features[i].properties.density;
-//     console.log(location);
-//     if (location) {
-//       heatArray.push([location[1], location[0], dense]);
-//     }
-//   }
+  for (var i = 0; i < response.features.length; i++) {
+    var location = response.features[i].geometry.coordinates;
+    var dense = response.features[i].properties.density;
+    //console.log(location);
+    if (location) {
+      heatArray.push([location[1], location[0], dense]);
+    }
+  }
 
-//   var heat = L.heatLayer(heatArray, {
-//     radius: 20,
-//     blur: 35
-//   }).addTo(myMap);
+  var heat = L.heatLayer(heatArray, {
+    radius: 20,
+    blur: 35
+  }).addTo(myMap);
+  console.log("test 1")
+});
+// switch case to set colors of heatmap
 
-// });
-// // switch case to set colors of heatmap
+// Marker layer
+var food = "static/data/fastfood.json";
+console.log(food)
+// Grab the data with d3
+d3.json(food).then((response1) =>{
+  console.log(response1);
 
-// // Marker layer
-// var food = "static/data/fastfood.json";
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup();
 
-// // Grab the data with d3
-// d3.json(food, function(response1) {
-//   console.log(response1);
+  // Loop through data
+  for (var i = 0; i < response1.features.length; i++) {
+    // Set the data location property to a variable
+    var location1 = response1.features[i].geometry.coordinates;
+    var rname = response1.features[i].properties.Big4;
+    switch(rname){
+      case "Burger King":
+        iconUrl = "static/images/bk.png";
+        iconSize = [80, 70]
+        break;
+      case "Taco Bell":
+        iconUrl = "static/images/tb.png";
+        iconSize = [40, 40]
+        break;
+      case "McDonalds":
+        iconUrl = "static/images/mcdonalds.png";
+        iconSize = [80, 70]
+        break;
+      case "Subway":
+        iconUrl = "static/images/subway.jpg";
+        iconSize = [40, 40]
+        break;
+      case "Wendys":
+        iconUrl = "static/images/wendys.png";
+        iconSize = [80, 70]
+        break;
+    }
+    var myIcon = L.icon({
+      iconUrl: iconUrl,
+      iconSize: iconSize
+      // iconAnchor: [22, 94],
+      // popupAnchor: [-3, -76]
+  });
 
-//   // Create a new marker cluster group
-//   var markers = L.markerClusterGroup();
+    // Check for location property
+    if (location1) {
 
-//   // Loop through data
-//   for (var i = 0; i < response1.features.length; i++) {
-//     // Set the data location property to a variable
-//     var location1 = response1.features[i].geometry.coordinates;
-//     var rname = response1.features[i].properties.Big4;
-//     switch(rname){
-//       case "Burger King":
-//         iconUrl = "static/images/bk.png";
-//         iconSize = [80, 70]
-//         break;
-//       case "Taco Bell":
-//         iconUrl = "static/images/tb.png";
-//         iconSize = [40, 40]
-//         break;
-//       case "McDonalds":
-//         iconUrl = "static/images/mcdonalds.png";
-//         iconSize = [80, 70]
-//         break;
-//       case "Subway":
-//         iconUrl = "static/images/subway.jpg";
-//         iconSize = [40, 40]
-//         break;
-//       case "Wendys":
-//         iconUrl = "static/images/wendys.png";
-//         iconSize = [80, 70]
-//         break;
-//     }
-//     var myIcon = L.icon({
-//       iconUrl: iconUrl,
-//       iconSize: iconSize
-//       // iconAnchor: [22, 94],
-//       // popupAnchor: [-3, -76]
-//   });
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([location1[1], location1[0]], {icon:myIcon})
+        .bindPopup("<h3> Name: " + response1.features[i].properties.Big4 + "</h3><hr><h3> City: " + response1.features[i].properties.city + "</h3>"));
+    }
 
-//     // Check for location property
-//     if (location1) {
+  }
 
-//       // Add a new marker to the cluster group and bind a pop-up
-//       markers.addLayer(L.marker([location1[1], location1[0]], {icon:myIcon})
-//         .bindPopup("<h3> Name :" + response1.features[i].properties.Big4 + "</h3><hr><h3> City: " + response1.features[i].properties.city + "</h3>"));
-//     }
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
 
-//   }
+})
+};
 
-//   // Add our marker cluster layer to the map
-//   myMap.addLayer(markers);
-
-// });
 
