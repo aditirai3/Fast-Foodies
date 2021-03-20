183be6187bd2bc4ad9f6deac9c972cc69f233351
Aditi Rai <aditi45@gmail.com>
2021-03-20 12:06:34 -0400

    updates

--- heatmap/static/js/heatmap.js
+++ heatmap/static/js/heatmap.js
@@ -1,10 +1,10 @@
 // create map object
-
 var myMap = L.map("map", {
   center: [37.0902, -95.7129],
   zoom: 7
 });
 
+// Add a tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -14,8 +14,11 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+// Heatmap layer
+// Assign the Data json to a variable
 var geodata = "static/data/cities.json";
 
+// Read the data using D3
 d3.json(geodata, function(response) {
 
   console.log(response.features.length);
@@ -39,8 +42,9 @@ d3.json(geodata, function(response) {
 });
 // switch case to set colors of heatmap
 
+// Marker layer
 var food = "static/data/fastfood.json";
-console.log("Hello");
+
 // Grab the data with d3
 d3.json(food, function(response1) {
   console.log(response1);
@@ -52,13 +56,42 @@ d3.json(food, function(response1) {
   for (var i = 0; i < response1.features.length; i++) {
     // Set the data location property to a variable
     var location1 = response1.features[i].geometry.coordinates;
+    var rname = response1.features[i].properties.Big4;
+    switch(rname){
+      case "Burger King":
+        iconUrl = "static/images/bk.png";
+        iconSize = [80, 70]
+        break;
+      case "Taco Bell":
+        iconUrl = "static/images/tb.png";
+        iconSize = [40, 40]
+        break;
+      case "McDonalds":
+        iconUrl = "static/images/mcdonalds.png";
+        iconSize = [80, 70]
+        break;
+      case "Subway":
+        iconUrl = "static/images/subway.jpg";
+        iconSize = [40, 40]
+        break;
+      case "Wendys":
+        iconUrl = "static/images/wendys.png";
+        iconSize = [80, 70]
+        break;
+    }
+    var myIcon = L.icon({
+      iconUrl: iconUrl,
+      iconSize: iconSize
+      // iconAnchor: [22, 94],
+      // popupAnchor: [-3, -76]
+  });
 
     // Check for location property
     if (location1) {
 
       // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([location1[1], location1[0]])
-        .bindPopup(response1.features[i].properties.Big4));
+      markers.addLayer(L.marker([location1[1], location1[0]], {icon:myIcon})
+        .bindPopup("<h3> Name :" + response1.features[i].properties.Big4 + "</h3><hr><h3> City: " + response1.features[i].properties.city + "</h3>"));
     }
 
   }
@@ -67,3 +100,4 @@ d3.json(food, function(response1) {
   myMap.addLayer(markers);
 
 });
+
--- heatmap/static/js/logic.js
+++ heatmap/static/js/logic.js
@@ -1,10 +1,10 @@
 // Creating map object
-
 var myMap = L.map("map", {
   center: [37.0902, -95.7129],
   zoom: 7
 });
 
+// Adding tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -14,34 +14,13 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
-console.log("Hello")
+var uscities = "static/data/cities.json";
 
-// Store API query variables
-var food = "static/data/fastfood.json";
-
-// Grab the data with d3
-d3.json(food, function(response) {
-
-  // Create a new marker cluster group
-  var markers = L.markerClusterGroup();
-
-  // Loop through data
-  for (var i = 0; i < response.length; i++) {
-    console.log(response.length)
-    // Set the data location property to a variable
-    var location = response.features[i].geometry.coordinates;
-
-    // Check for location property
-    if (location) {
-
-      // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([location[1], location[0]])
-        .bindPopup(response.features[i].properties.Big4));
-    }
-
-  }
-
-  // Add our marker cluster layer to the map
-  myMap.addLayer(markers);
-
+uscities.forEach(function(state) {
+  var polygon = L.polygon(state.geometry.coordinates, {
+      weight: 1,
+      fillOpacity: 0.7,
+      color: 'white',
+      dashArray: '3'
+  }).addTo(map);
 });
